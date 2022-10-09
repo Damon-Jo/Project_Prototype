91a97985d495074ea7350f8042d041082b6eb385
Damon-Jo <lunaticjky@gmail.com>
2022-10-09 03:20:35 -0700

    created other features

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Detail from './routes/Detail.js';
 import ProjectInfo from './routes/ProjectInfo.js';
 import Demo from './routes/Demo.js';
+import OtherFeatures from './routes/OtherFeatures.js';
 import Input from './components/Input.js';
 
 import { Routes, Route} from "react-router-dom";
@@ -18,6 +19,7 @@ function App() {
         <Route path="/ProjectInformation/" element={<ProjectInfo/>}/>
         
         <Route path="/demo/" element={<Demo/>}/>
+        <Route path="/otherFeatures/" element={<OtherFeatures/>}/>
 {/* 
         <Route>
           <Demo>
---
+++ src/data/otherFeaturesData.js
@@ -0,0 +1,24 @@
+let otherFeaturesdata = [
+    {
+        title : 'Building has outdoor air ventilation',
+        type : "text"
+    },
+    {
+        title : 'Building has tenant common laundry',
+        type : "text"
+    },
+    {
+        title : 'Building has central commercial laundry',
+        type : "text"
+    },
+    {
+        title : 'Building has commercial kitchen',
+        type : "text"
+    },
+    {
+        title : 'Electricity paid by tenants?',
+        type : "text"
+    }
+]
+
+export default otherFeaturesdata;
